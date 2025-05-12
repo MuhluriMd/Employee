89e94020235265a45f8b91a7d0891ba6605b2e81
muhluri_md <mudakamuhluri@gmail.com>
2025-05-12 23:32:18 +0200

    EJB Upload

---
+++ ManagerEJBModule/ManagerEJBModule/build/generated-sources/ap-source-output/ac/za/tut/entities/Employee_.java
@@ -0,0 +1,15 @@
+package ac.za.tut.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-10T22:44:29")
+@StaticMetamodel(Employee.class)
+public class Employee_ { 
+
+    public static volatile SingularAttribute<Employee, Long> id;
+    public static volatile SingularAttribute<Employee, String> department;
+    public static volatile SingularAttribute<Employee, Double> salary;
+
+}
